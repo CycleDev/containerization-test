acea2d01cb57ee6f72f443621ee54b631c2cdc53
CycleDev <oprysk.nazar@gmail.com>
2018-05-14 22:50:31 -0600

    Added hello world controller.

---
+++ src/main/java/nao/cycledev/container/containerizationtest/ContainerizationTestApplication.java
@@ -0,0 +1,12 @@
+package nao.cycledev.container.containerizationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerizationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerizationTestApplication.class, args);
+    }
+}
---
+++ src/main/java/nao/cycledev/container/containerizationtest/HelloWorldController.java
@@ -0,0 +1,13 @@
+package nao.cycledev.container.containerizationtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/say/hello")
+    public String greetings() {
+        return "Hello World!!!";
+    }
+}
